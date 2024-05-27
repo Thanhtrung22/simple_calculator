3377fffac2625be762a54919415cb357cdf49f36
ThanhTrung22 <anhtrungpk11@gmail.com>
2024-05-27 19:51:49 +0700

    complete v1.0

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 ### 1. Calculate basic
 ### 2. Find GCD
 ### 3. Exit
+
+### Current version v1.0
+- Functions:
+    -Basic calculation
+    -Find GCD
+    -Exit
+-Bugs:
+    - If input is not a number, program  will run infinite
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
+#include <ctype.h>
 uint32_t add(uint32_t a, uint32_t b);
 uint32_t minus(uint32_t a, uint32_t b);
 float div(uint32_t a, uint32_t b);
@@ -8,58 +9,58 @@ uint64_t mul(uint32_t a, uint32_t b);
 void menu();
 void input(uint32_t *a, uint32_t *b);
 uint32_t GCD(uint32_t a, uint32_t b);
+
 int main()
 {
     uint8_t select = 0;
     char operand = 0;
     uint32_t a, b;
-    while(1)
+    while (1)
     {
-        //system("cls");
+        // system("cls");
         menu();
         printf("\nNhap lua chon cua ban: ");
         scanf("%hhu", &select);
-        switch(select)
+        switch (select)
         {
-            case 1:
+        case 1:
+        {
+            input(&a, &b);
+            printf("Nhap phep toan: ");
+            while ((getchar()) != '\n')
+                ;
+            scanf("%c", &operand);
+            switch (operand)
             {
-                input(&a, &b);
-                printf("Nhap phep toan: ");
-                while((getchar()) !='\n');
-                scanf("%c", &operand);
-                switch(operand)
-                {
-                    case '+':
-                        printf("%u + %u = %u", a, b, add(a, b));
-                        break;
-                    case '-':
-                        printf("%u - %u = %d", a, b, minus(a, b));
-                        break;
-                    case '*':
-                        printf("%u * %u = %llu", a, b, mul(a, b));
-                        break;
-                    case '/':
-                        printf("%u / %u = %.2f", a, b, div(a, b));
-                        break;
-                    default: 
-                        printf("\nNhap sai phep toan");
-                }
+            case '+':
+                printf("%u + %u = %u", a, b, add(a, b));
                 break;
-            }
-            case 2:
-            {
-                input(&a, &b);
-                printf("UCLN cua %u va %u la %u", a, b, GCD(a,b));
+            case '-':
+                printf("%u - %u = %d", a, b, minus(a, b));
                 break;
+            case '*':
+                printf("%u * %u = %llu", a, b, mul(a, b));
+                break;
+            case '/':
+                printf("%u / %u = %.2f", a, b, div(a, b));
+                break;
+            default:
+                printf("\nNhap sai phep toan");
             }
-            case 3:
-            {
-                printf("\nSee you again");
-                return 0;
-            }
+            break;
         }
-
-
+        case 2:
+        {
+            input(&a, &b);
+            printf("UCLN cua %u va %u la %u", a, b, GCD(a, b));
+            break;
+        }
+        case 3:
+        {
+            printf("\nSee you again");
+            return 0;
+        }
+        }
     }
     return 0;
 }
@@ -80,7 +81,6 @@ void input(uint32_t *a, uint32_t *b)
     
     printf("Nhap so thu hai: ");
     scanf("%u", b);
-    
 }
 
 uint32_t add(uint32_t a, uint32_t b)
@@ -100,7 +100,7 @@ uint64_t mul(uint32_t a, uint32_t b)
 
 float div(uint32_t a, uint32_t b)
 {
-    
+
     return (float)a / (float)b;
 }
 
@@ -108,10 +108,10 @@ uint32_t GCD(uint32_t a, uint32_t b)
 {
     uint32_t ucln = 0;
     uint32_t min = a < b ? a : b;
-    
-    for(uint32_t i = min; i >= 1; i--)
+
+    for (uint32_t i = min; i >= 1; i--)
     {
-        if(a % i == 0 && b % i == 0)
+        if (a % i == 0 && b % i == 0)
         {
             ucln = i;
             break;
