e9a04236c86c78ee5d68c56b428528d8b85c6095
ThanhTrung22 <anhtrungpk11@gmail.com>
2024-05-27 18:38:45 +0700

    modify GCD for quick run

--- main.c
+++ main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <math.h>
-int32_t add(int32_t a, int32_t b);
-int32_t minus(int32_t a, int32_t b);
-float div(int32_t a, int32_t b);
-int32_t mul(int32_t a, int32_t b);
+uint32_t add(uint32_t a, uint32_t b);
+uint32_t minus(uint32_t a, uint32_t b);
+float div(uint32_t a, uint32_t b);
+uint64_t mul(uint32_t a, uint32_t b);
 void menu();
-void input(int32_t *a, int32_t *b);
-int32_t GCD(int32_t a, int32_t b);
-int32_t main()
+void input(uint32_t *a, uint32_t *b);
+uint32_t GCD(uint32_t a, uint32_t b);
+int main()
 {
     uint8_t select = 0;
     char operand = 0;
-    int32_t a, b;
+    uint32_t a, b;
     while(1)
     {
         //system("cls");
@@ -30,16 +30,16 @@ int32_t main()
                 switch(operand)
                 {
                     case '+':
-                        printf("%d + %d = %d", a, b, add(a, b));
+                        printf("%u + %u = %u", a, b, add(a, b));
                         break;
                     case '-':
-                        printf("%d - %d = %d", a, b, minus(a, b));
+                        printf("%u - %u = %d", a, b, minus(a, b));
                         break;
                     case '*':
-                        printf("%d * %d = %d", a, b, mul(a, b));
+                        printf("%u * %u = %llu", a, b, mul(a, b));
                         break;
                     case '/':
-                        printf("%d / %d = %.2f", a, b, div(a, b));
+                        printf("%u / %u = %.2f", a, b, div(a, b));
                         break;
                     default: 
                         printf("\nNhap sai phep toan");
@@ -73,45 +73,48 @@ void menu()
     printf("\n\t2.Find GCD");
     printf("\n\t3. Exit");
 }
-void input(int32_t *a, int32_t *b)
+void input(uint32_t *a, uint32_t *b)
 {
     printf("Nhap so thu nhat: ");
-    scanf("%d", a);
+    scanf("%u", a);
+    
     printf("Nhap so thu hai: ");
-    scanf("%d", b);
+    scanf("%u", b);
+    
 }
 
-int32_t add(int32_t a, int32_t b)
+uint32_t add(uint32_t a, uint32_t b)
 {
     return a + b;
 }
 
-int32_t minus(int32_t a, int32_t b)
+uint32_t minus(uint32_t a, uint32_t b)
 {
     return a - b;
 }
 
-int32_t mul(int32_t a, int32_t b)
+uint64_t mul(uint32_t a, uint32_t b)
 {
     return a * b;
 }
 
-float div(int32_t a, int32_t b)
+float div(uint32_t a, uint32_t b)
 {
     
     return (float)a / (float)b;
 }
 
-int32_t GCD(int32_t a, int32_t b)
+uint32_t GCD(uint32_t a, uint32_t b)
 {
-    int32_t ucln = 0;
-    int32_t min = a < b ? a : b;
+    uint32_t ucln = 0;
+    uint32_t min = a < b ? a : b;
     
-    for(int32_t i = 1; i <= abs(min); i++)
+    for(uint32_t i = min; i >= 1; i--)
     {
         if(a % i == 0 && b % i == 0)
         {
             ucln = i;
+            break;
         }
     }
     return ucln;
